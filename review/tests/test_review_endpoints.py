2e6f22ebf8a9d19b23e3d84719d27d6346cf8172
Michael Florendo <michael.florendo.002@gmail.com>
2025-05-05 21:02:10 +0800

    add test for review

--- review/tests/test_review_endpoints.py
+++ review/tests/test_review_endpoints.py
@@ -13,13 +13,73 @@ class TestReviewPublicEndpoints(TestCase):
     def setUp(self):
         self.client = APIClient()
         self.user = create_user()
+        self.shop = create_shop(self.user)
+        self.product = create_product(user=self.user, shop=self.shop)
 
     def test_get_review(self):
         """should list reviews of a product"""
-        shop = create_shop(self.user)
-        product = create_product(user=self.user, shop=shop)
-        create_review(self.user, product, message='foo', created_by=self.user)
-        create_review(self.user, product, message='bar', created_by=self.user)
+        create_review(self.user, self.product, message='foo', created_by=self.user)
+        create_review(self.user, self.product, message='bar', created_by=self.user)
         res = self.client.get(REVIEWS_URL)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data['results']), 2)
+
+    def test_unauthorized_create_review(self):
+        """
+        GIVEN an unauthenticated user
+        WHEN the user tries to create review
+        THEN an Unauthorized error should return
+        """
+        payload = dict(message='foo', rating='4.5')
+        res = self.client.post(REVIEWS_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+@pytest.mark.django_db
+class TestReviewPrivateEndpoints(TestCase):
+
+    def setUp(self):
+        self.user = create_user()
+        self.client = APIClient()
+        self.shop = create_shop(user=self.user)
+        self.product = create_product(user=self.user, shop=self.shop)
+        self.client.force_authenticate(user=self.user)
+
+    def test_create_review(self):
+        payload = dict(message='hello world', rating='4.5', product=self.product.id)
+        res = self.client.post(REVIEWS_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+    def test_create_review_blank_message(self):
+        """
+        GIVEN a review payload with no message
+        WHEN a user creates a review
+        THEN a review should be created with blank message
+        """
+        payload = dict(rating='4.5', product=self.product.id)
+        res = self.client.post(REVIEWS_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        review = res.data
+        self.assertEqual(review['message'], '')
+
+    def test_create_review_with_invalid_rating(self):
+        """
+        GIVEN a review payload with a rating greater than 5
+        WHEN a user creates the review
+        THEN a validation error should return
+        """
+        payload = dict(rating=6, product=self.product.id)
+
+        res = self.client.post(REVIEWS_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('rating', res.data)
+
+    def test_create_review_with_invalid_rating1(self):
+        """
+        GIVEN a review payload with a rating less than 0
+        WHEN a user creates the review
+        THEN a validation error should return
+        """
+        payload = dict(rating=-1, product=self.product.id)
+        res = self.client.post(REVIEWS_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('rating', res.data)
--- review/views.py
+++ review/views.py
@@ -26,6 +26,7 @@ class ReviewAPIView(views.APIView):
     def post(self, request):
         serializer = ReviewCreateSerializer(data=request.data)
         if serializer.is_valid():
-            review = serializer.save(created_by=self.request.user)
+            review = serializer.save(user=self.request.user, created_by=self.request.user)
             response_serializer = ReviewSerializer(review)
             return Response(response_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
